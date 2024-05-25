59a2f137764bc064cf194510259340b2c4a70fff
Larysa Rud <larpavrud@gmail.com>
2024-05-24 22:33:22 -0400

    Creating a server application with Node and Express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Express server app for cataloguing my home library");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const path = require('path');
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
